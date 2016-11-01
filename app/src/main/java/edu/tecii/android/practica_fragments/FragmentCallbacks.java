c078a29fe6c855f6dfea9f8f85cd78b8cfc0671e
HazukiS <alejandracalderon322@gmail.com>
2016-10-31 23:38:23 -0700

    Initial commit

---
+++ app/src/main/java/edu/tecii/android/practica_fragments/FragmentCallbacks.java
@@ -0,0 +1,9 @@
+package edu.tecii.android.practica_fragments;
+
+/**
+ * Created by hazuk on 31/10/2016.
+ */
+public interface FragmentCallbacks {
+    public void onMsgFromMainToFragment(String strValue);
+}
+
---
+++ app/src/main/java/edu/tecii/android/practica_fragments/MainCallbacks.java
@@ -0,0 +1,8 @@
+package edu.tecii.android.practica_fragments;
+
+/**
+ * Created by hazuk on 31/10/2016.
+ */
+public interface MainCallbacks {
+    public void onMsgFromFragToMain (String sender,String strValue);
+}
